fd1807b8bbec060d9c6151d2a75b6f78764281af
Aaron Cox <i.aaroncox@gmail.com>
2020-08-18 00:39:17 -0400

    add reviewer notes. polish up publish script

--- publish.sh
+++ publish.sh
@@ -1,9 +1,17 @@
 #! /bin/bash
 
+# housekeeping
 rm -fr ./publish ./dist
 mkdir ./publish
 
+# set up the source README for extension reviewers
+sed -e "s/\[\[npm-version\]\]/$(npm --version)/" < reviewer-notes.md | sed -e "s/\[\[node-version\]\]/$(node --version)/" > README
+
+# package up source code
+zip -r ./publish/bb-booster.source.zip package.json npm-shrinkwrap.json src tsconfig.json README
+
+# build and pack extension
 npm run-script build
 
-zip -r ./publish/bb-booster.source.zip package.json npm-shrinkwrap.json src tsconfig.json
-zip -r ./publish/bb-booster.zip dist/*
+cd ./dist
+zip -r ../publish/bb-booster.zip ./*
---
+++ reviewer-notes.md
@@ -0,0 +1,18 @@
+
+# Build Environment
+
+This build was produced with the following software versions:
+
+- node [[node-version]]
+- npm [[npm-version]]
+
+# Build Instructions
+
+1. unzip bb-booster.source.zip
+2. npm install
+3. npm run-script build
+
+The extension files will be located in ./dist
+
+- 
+- 
