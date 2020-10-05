2f1cfbe38c69f3e9a51c9654ebf1966298ba092c
Aaron Cox <i.aaroncox@gmail.com>
2020-10-04 22:06:45 -0400

    start adding training tracking

---
+++ src/content-scripts/training/training.tsx
@@ -0,0 +1,15 @@
+
+import {h, Component,  render} from "preact";
+
+class Training extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
